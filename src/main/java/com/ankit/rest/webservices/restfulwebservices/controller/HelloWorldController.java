c2805a50f55c70e0b10113fbbd83c9962b26983b
Ankit dhaka <ankitdhaka2807@gmail.com>
2023-12-31 21:59:01 +0530

    Request Mapping Changed to GetMapping

--- src/main/java/com/ankit/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/ankit/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.ankit.rest.webservices.restfulwebservices.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     String helloWorld() {
         return "Hello World!!!";
     }
